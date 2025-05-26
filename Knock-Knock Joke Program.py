ef6679e193e2249761a3a2cc135f5fddc3d9ddc3
Ben Sherman <bsherman170@students.mchenry.edu>
2025-05-26 13:01:31 -0500

    Added python knock knock joke for conversation.

---
+++ Knock-Knock Joke Program.py
@@ -0,0 +1,9 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")  
+print("Who's there?")  
+print("Pineapple.")  
+print("Pineapple who?")  
+print("Pine aren't you an apple!")  
+
+# As one line of code using escape characters
+print("Knock, knock.\nWho's there?\nPineapple.\nPineapple who?\nPine aren't you an apple!")
