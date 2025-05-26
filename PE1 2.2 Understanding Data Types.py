39bd46f53347fae865bc66a1c19e74ee49d3f7b5
Ben Sherman <bsherman170@students.mchenry.edu>
2025-05-26 13:06:39 -0500

    Understanding Data types program

---
+++ PE1 2.2 Understanding Data Types.py
@@ -0,0 +1,21 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 25)
+
+# Integer
+y = -30
+print("Integer:", y)
+
+# Float
+z = 5
+print("Float:", z)
+
+# String
+name = "Billy"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
