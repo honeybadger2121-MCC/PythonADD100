27b8ac9c3b0c0cbab9739eedbd9c87c6abd8cccd
Ben Sherman <bsherman170@students.mchenry.edu>
2025-05-26 14:17:47 -0500

    Converting Kilograms to Pounds

---
+++ PE1 2.4 Converting kilograms to pounds.py
@@ -0,0 +1,20 @@
+#Converting kilograms to pounds
+kg1= 200
+kg2 = 551
+kg3 = 132
+kg4 = 100
+kg5 = 50
+conversionMultiplier = 2.20462
+#Pounds (lb) = Kilograms (kg) * 2.20462 (conversionMultiplier)
+kg1total = kg1*conversionMultiplier
+kg2total = kg2*conversionMultiplier
+kg3total = kg3*conversionMultiplier
+kg4total = kg4*conversionMultiplier
+kg5total = kg5*conversionMultiplier
+
+print(kg1, "kilograms", "is equal to", kg1total , "pounds" )
+print(kg2, "kilograms", "is equal to", kg2total , "pounds" )
+print(kg3, "kilograms", "is equal to", kg3total , "pounds" )
+print(kg4, "kilograms", "is equal to", kg4total , "pounds" )
+print(kg5, "kilograms", "is equal to", kg4total , "pounds" )
+
