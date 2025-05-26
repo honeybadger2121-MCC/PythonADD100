8271b58461d3b515d787fd0a68eb47cb4d28173d
Ben Sherman <bsherman170@students.mchenry.edu>
2025-05-26 13:12:40 -0500

    Mathematical Operations Program

---
+++ PE1 2.3 Mathematical Operations.py
@@ -0,0 +1,12 @@
+# Perform basic mathematical operations
+length = 20
+width = 10
+
+area = length * width
+
+
+print("Length:", length , "inches")
+print("Width:", width, "inches")
+print("Area:", area , "inches")
+
+print("The total Area of the shape is:", area, "inches")
