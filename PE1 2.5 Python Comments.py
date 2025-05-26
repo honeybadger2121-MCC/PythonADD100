028e7424f88e282ab84af9ed5c68513c68412efd
Ben Sherman <bsherman170@students.mchenry.edu>
2025-05-26 14:22:00 -0500

    Python Commenting

---
+++ PE1 2.5 Python Comments.py
@@ -0,0 +1,11 @@
+#this program calculates what your miles per gallon is
+tank_size = 15
+#how many gallons of fuel your tank can hold 
+# Define the total distance covered
+distance = 600
+#how many miles you traveled
+# Calculate miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+#this prints the results and tells you what your miles per gallon was
